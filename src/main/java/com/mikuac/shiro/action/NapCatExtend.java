c3955aeadf728da3a7c11aab342654d1738ed911
飒翊 <42363060+sa-yi@users.noreply.github.com>
2026-08-07 10:06:13 +0800

    :sparkles: Parse forwarded messages with oversized msg_id from NapCat #407
    NapCatExtend 接口新增 getForwardMsg(String msgId)
    Bot 实现 getForwardMsg(String msgId)，使用 ACTION_PATH.GET_FORWARD_MSG
    
    Co-authored-by: MisakaTAT <i@mikuac.com>

--- src/main/java/com/mikuac/shiro/action/NapCatExtend.java
+++ src/main/java/com/mikuac/shiro/action/NapCatExtend.java
@@ -2,6 +2,7 @@ package com.mikuac.shiro.action;
 
 import com.mikuac.shiro.dto.action.common.ActionData;
 import com.mikuac.shiro.dto.action.common.ActionRaw;
+import com.mikuac.shiro.dto.action.response.GetForwardMsgResp;
 import com.mikuac.shiro.dto.action.response.GetMsgListResp;
 import com.mikuac.shiro.dto.action.response.SendQzoneResp;
 
@@ -47,6 +48,12 @@ public interface NapCatExtend {
      */
     ActionRaw setMsgEmojiLike(int msgId, String code, boolean isSet);
 
+    /**
+     * 获取合并转发消息，使用String类型的msgId
+     * @param msgId - 消息ID
+     * @return result {@link ActionData} of {@link GetForwardMsgResp}
+     */
+    ActionData<GetForwardMsgResp> getForwardMsg(String msgId);
 
     /**
      * 发表QQ空间说说
